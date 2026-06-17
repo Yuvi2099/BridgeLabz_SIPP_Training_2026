601bae00e4f0a7b76702ce3063ff1091b947d043
Yuvraj <yuvraj.singh0317@gmail.com>
2026-06-17 10:19:11 +0530

    [YUVRAJ]:added the solutions of level 1 of string

---
+++ CoreJavaPractice/gcr_codebase/String/Level_1/NullPointerDemo.java
@@ -0,0 +1,31 @@
+package CoreJavaPractice.gcr_codebase.String.Level_1;
+public class NullPointerDemo {
+
+    
+    public static void generateException() {
+
+        String text = null;
+
+        System.out.println(text.length());
+    }
+
+    
+    public static void handleException() {
+
+        String text = null;
+
+        try {
+            System.out.println(text.length());
+        }
+        catch (NullPointerException e) {
+            System.out.println("NullPointerException handled successfully.");
+            System.out.println("Message: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+
+    
+        handleException();
+    }
+}
