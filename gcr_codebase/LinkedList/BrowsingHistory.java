bb4fddd2972de7d83cda972fee4c9bf3b59aa892
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-22 13:14:04 +0530

    [YUVRAJ]:added the Linked List Solutions

---
+++ gcr_codebase/LinkedList/BrowsingHistory.java
@@ -0,0 +1,53 @@
+package gcr_codebase.LinkedList;
+import java.util.*;
+public class BrowsingHistory {
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static Node reverseHistory(Node head) {
+
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+
+            Node next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    static void print(Node head) {
+
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        head = reverseHistory(head);
+
+        print(head);
+    }
+}
