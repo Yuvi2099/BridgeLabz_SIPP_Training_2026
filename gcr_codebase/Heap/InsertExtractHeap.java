43bf30274e0eb98f245adc81882e5bbcd4b59d1c
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-23 09:31:18 +0530

    [YUVRAJ]:added the Heap Solutions

---
+++ gcr_codebase/Heap/InsertExtractHeap.java
@@ -0,0 +1,48 @@
+package gcr_codebase.Heap;
+import java.util.PriorityQueue;
+public class InsertExtractHeap {
+
+    static class Patient {
+        String name;
+        int severity;
+
+        Patient(String name, int severity) {
+            this.name = name;
+            this.severity = severity;
+        }
+
+        @Override
+        public String toString() {
+            return name + " (Severity: " + severity + ")";
+        }
+    }
+
+    static PriorityQueue<Patient> triageQueue =
+            new PriorityQueue<>((a, b) -> b.severity - a.severity);
+
+    public static void addPatient(String name, int severity) {
+        triageQueue.offer(new Patient(name, severity));
+    }
+
+    // Extract highest priority patient
+    public static Patient treatNext() {
+        if (triageQueue.isEmpty()) {
+            return null;
+        }
+        return triageQueue.poll();
+    }
+
+    public static void main(String[] args) {
+
+        addPatient("Rahul", 5);
+        addPatient("Aman", 9);
+        addPatient("Priya", 7);
+        addPatient("Riya", 3);
+
+        System.out.println("Treatment Order:");
+
+        while (!triageQueue.isEmpty()) {
+            System.out.println(treatNext());
+        }
+    }
+}
---
+++ gcr_codebase/Heap/KthLargest.java
@@ -0,0 +1,31 @@
+package gcr_codebase.Heap;
+import java.util.PriorityQueue;
+public class KthLargest {
+
+    public static int findKthLargest(int[] scores, int k) {
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int score : scores) {
+
+            if (minHeap.size() < k) {
+                minHeap.offer(score);
+            } else if (score > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(score);
+            }
+        }
+
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+
+        int[] scores = {55, 72, 90, 81, 65, 99, 78, 88};
+        int k = 3;
+
+        int ans = findKthLargest(scores, k);
+
+        System.out.println(k + "rd Largest Element = " + ans);
+    }
+}
